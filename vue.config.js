4fc761eafbf799e12e0576231b0aa7ce6c5a9743
crlkg <102585701+crlkg@users.noreply.github.com>
2025-04-07 13:36:34 +0900

    repo 입력

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service');
 const webpack = require('webpack');
 
 module.exports = defineConfig({
+  publicPath: 'https://github.com/crlkg/BooK.git',
   transpileDependencies: true,
   configureWebpack: {
     plugins: [
