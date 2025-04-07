c0bb93d83a4dd827c0bc4ee7778ea2610c91ff1a
crlkg <102585701+crlkg@users.noreply.github.com>
2025-04-07 13:47:03 +0900

    레포 이름

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service');
 const webpack = require('webpack');
 
 module.exports = defineConfig({
-  publicPath: 'BooK',
+  publicPath: '/BooK/',
   transpileDependencies: true,
   configureWebpack: {
     plugins: [
